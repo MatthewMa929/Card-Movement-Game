5486efc6c165834ca4876e547027a071b2960416
MatthewMa929 <MatthewMa929@Gmail.com>
2024-07-09 08:53:10 -0400

    tilemap

--- Game/map.gd
+++ Game/map.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
-@onready var tilemap = $TileMap
+@onready var tilemap = $TileMap	
 @onready var under = $Under
 @onready var over = $Over
+@onready var y_offset = 10
+@onready var cell_size = 32
 
 func _ready():
 	pass
@@ -10,7 +12,14 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_just_pressed("Left Click"):
-		pass
+		var mouse_vectori = create_vectori(get_global_mouse_position())
+		if tilemap.get_cell_atlas_coords(0, mouse_vectori) == Vector2i(1, 0):
+			tilemap.set_cell(0, mouse_vectori, 0, Vector2i(0, 0))
+
+func create_vectori(vector):
+	if vector.y < y_offset:
+		return Vector2i(-1, -1)
+	return Vector2i(vector.x/cell_size, (vector.y-y_offset)/cell_size)
 
 func create_stage():
 	var pos = Vector2(-32, 1)
